64355fcc9bc5393af2a167da92b60cb9aa22e7c1
VictorBK <victorbrian4@gmail.com>
2022-12-14 08:53:19 +0300

    What a dagnerous front 3!

---
+++ Coding-Bat/JAVA/Warmup-1/front3.java
@@ -0,0 +1,9 @@
+public String front3(String str) {
+  String front;
+    if(str.length() < 3)
+        front = str;
+    else
+        front = str.substring(0, 3);
+                   
+    return front + front + front;
+}
