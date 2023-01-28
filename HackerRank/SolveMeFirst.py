6a8535ee44a114a21d23d6eae65bc04831f2359d
VictorBK <victorbrian4@gmail.com>
2023-01-28 11:32:16 +0300

    First py in hackerrank

---
+++ HackerRank/SolveMeFirst.py
@@ -0,0 +1,11 @@
+#Complete the function solveMeFirst to compute the sum of two integers
+#solveMeFirst has parameters int a (first value) and int b (second value)
+#function int returns the sum of a and b
+
+def solveMeFirst(a, b):
+    return a + b
+
+    num1 = int(input())
+    num2 = int(input())
+    ans = solveMeFirst(num1,num2)
+    print(ans)
