becb386430c5e471d7e022936ef7de8b1847060e
VictorBK <victorbrian4@gmail.com>
2023-02-03 17:11:10 +0300

    Time conversion complexities.

---
+++ HackerRank/TimeConversion.py
@@ -0,0 +1,45 @@
+# Given a time in 12-hour AM/PM format
+# convert it to military (24-hour) time.
+# Note:- 12:00:00AM on a 12-hour clock is 00:00:00 on a 24-hour clock.
+# Note:- 12:00:00PM on a 12-hour clock is 12:00:00 on a 24-hour clock.
+
+# Example: s = '12:01:00 PM' Return '12:01:00'
+# Example: s = '12:01:00 AM' Return '00:01:00'
+
+#!/usr/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'timeConversion' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts STRING s as parameter.
+#
+
+def timeConversion(s):
+    if s[-2:] == "AM" and s[:2] == "12":
+        return "00" + s[2:-2]
+    elif s[-2:] == "AM":
+        return s[:-2]
+    elif s[-2:] == "PM" and s[:2] == "12":
+        return s[:-2]
+    else:
+        ans = int(s[:2]) + 12
+        return str(str(ans) + s[2:8])
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = timeConversion(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
+    
