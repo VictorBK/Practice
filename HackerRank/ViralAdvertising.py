bc9385c210a3dda16ba9e94630aa12efbaed0143
VictorBK <victorbrian4@gmail.com>
2023-04-04 22:12:06 +0300

    Viral Advertising

---
+++ HackerRank/ViralAdvertising.py
@@ -0,0 +1,37 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'viralAdvertising' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER n as parameter.
+#
+
+def viralAdvertising(n):
+    # Write your code here
+    count = 0
+    people = 5
+
+    for x in range(n):
+        people = people // 2
+        count += people
+        people = people * 3
+
+    return count
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    result = viralAdvertising(n)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
