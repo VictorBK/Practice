a5c714cdf23f52c29521ee2463ed596fa5d0d2d7
VictorBK <victorbrian4@gmail.com>
2023-04-05 23:43:49 +0300

    Counting Valleys

---
+++ HackerRank/CountingValleys.py
@@ -0,0 +1,41 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'countingValleys' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER steps
+#  2. STRING path
+#
+
+def countingValleys(steps, path):
+    # Write your code here
+    altitude = 0
+    no_of_valley = 0
+    for step in path:
+        if step == "U": altitude+=1 
+        else: altitude-=1
+
+        if step == "U" and altitude == 0:
+            no_of_valley += 1
+    return no_of_valley
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    steps = int(input().strip())
+
+    path = input()
+
+    result = countingValleys(steps, path)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
