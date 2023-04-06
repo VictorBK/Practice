437a34e4579c9994cd847143efca84d2eaae2efd
VictorBK <victorbrian4@gmail.com>
2023-04-06 16:22:40 +0300

    Prisoner saved

---
+++ HackerRank/SaveThePrisoner.py
@@ -0,0 +1,51 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'saveThePrisoner' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER m
+#  3. INTEGER s
+#
+
+def saveThePrisoner(n, m, s):
+    # Write your code here
+    remainder = m % n
+    lastPosition = 0
+    if(s > 1):
+        lastPosition = remainder + s - 1
+    else:
+        lastPosition = remainder + n
+    
+    if lastPosition > n:
+        lastPosition = lastPosition - n
+
+    return lastPosition
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        first_multiple_input = input().rstrip().split()
+
+        n = int(first_multiple_input[0])
+
+        m = int(first_multiple_input[1])
+
+        s = int(first_multiple_input[2])
+
+        result = saveThePrisoner(n, m, s)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
